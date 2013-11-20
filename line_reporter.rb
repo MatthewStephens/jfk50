fed9a5d34c052b3c8c7f1805a46454f27ca35c03
Matthew Stephens <mattbastard@gmail.com>
2013-11-20 09:02:03 -0500

    utility script for checking CSV

---
+++ line_reporter.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+
+@limit = ARGV[0].to_i || 120
+
+tweet_data=[]
+
+CSV.foreach("./TeletypeForTweeting.csv") do |row| 
+  tweet_data << row.to_csv.chop
+end
+
+def parse(row)
+  one, two, three = "", "", ""
+  match = row.match(/^([^,]*),([^,]*),(.*)$/)
+  if ! match.nil?
+    one = match[1] || ""
+    two = match[2] || ""
+    three = match[3] || ""
+  end
+  return one, two, three
+end
+
+tweet_data.each_with_index do |line,index| 
+  code,timestamp,content=parse(line)
+  puts "#{index} #{content}" if content.length > @limit
+end
+puts "rows with tweets longer than #{@limit}"
