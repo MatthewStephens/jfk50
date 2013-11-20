c5d0d87d5046193eaf95fce99647170746b7c831
Matthew Stephens <mattbastard@gmail.com>
2013-11-20 16:13:14 -0500

    Fixing bug in timestamp correction routine.
    Also moving over to different testing account.

--- destroyer.rb
+++ destroyer.rb
@@ -3,16 +3,23 @@
 require 'date'
 require 'twitter'
 
-CONSUMER_KEY="MtKiWMxQiGoMkerKq9blpg"
-CONSUMER_SECRET="ldBbmasgnqixO0XDoOmsRz218iMhyl3ubnvebbtxkpQ"
 REQ_TOKEN_URL="https://api.twitter.com/oauth/request_token"
 AUTH_URL="https://api.twitter.com/oauth/authorize"
 ACCESS_TOKEN_URL="https://api.twitter.com/oauth/access_token"
 
-ACCESS_TOKEN="2190727249-h0G0u3ZUbHRJ80qQIGekZG4mbP0bnzXhqvyGbNP"
-ACCESS_TOKEN_SECRET="q11BgBbI5ixbjHzJ80VeBiCnTZUGTC6olYCyH2O2Xuvus"
+# first app
+CONSUMER_KEY="7IqHUKOlricIrj86PeN3g"
+CONSUMER_SECRET="R0RPXCwYX84UHuXTkNgHTlIG8od848Q3eAvDHbAgOs"
+ACCESS_TOKEN="2205397134-qn70xoG2nQB7Oub5IGgTlJVOnYGFqkpKmCvPGvz"
+ACCESS_TOKEN_SECRET="TYrVnuRG5TY5MJmrMvIRtlphpwfLs5bQnzpdDyAwFoX8i"
 
-username="Henry H. Lightcap"
+# second app
+ALTERNATE_CONSUMER_KEY="1pmJzZAnNKxa2BTnl3oug"
+ALTERNATE_CONSUMER_SECRET="yHLeG21TjW4ixQF6q5eRz9I7zcwOP0PFAoxWEkGbs"
+ALTERNATE_ACCESS_TOKEN="2205397134-Sv8bK6ZSXc9hTn3iAUZaEG9F5sltewKSmYr62no"
+ALTERNATE_ACCESS_TOKEN_SECRET="5jl6OPmEWe7DxOVHOzryEXS3XuodFjVIZX5oeYsS7EjQR"
+
+username="Whimsy Bafflegab"
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = CONSUMER_KEY
@@ -20,6 +27,14 @@ client = Twitter::REST::Client.new do |config|
   config.access_token        = ACCESS_TOKEN
   config.access_token_secret = ACCESS_TOKEN_SECRET
 end
+
+backup_client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ALTERNATE_CONSUMER_KEY
+  config.consumer_secret     = ALTERNATE_CONSUMER_SECRET
+  config.access_token        = ALTERNATE_ACCESS_TOKEN
+  config.access_token_secret = ALTERNATE_ACCESS_TOKEN_SECRET
+end
+
 
 timeline=client.user_timeline(username, :count => 200)
 list = timeline.map(&:id)
