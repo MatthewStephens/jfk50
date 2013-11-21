e564a6fc789431ab7a9461bdce7d754d3d949f96
Matthew Stephens <mattbastard@gmail.com>
2013-11-21 11:49:59 -0500

    adding keys for this account

--- destroyer.rb
+++ destroyer.rb
@@ -8,18 +8,13 @@ AUTH_URL="https://api.twitter.com/oauth/authorize"
 ACCESS_TOKEN_URL="https://api.twitter.com/oauth/access_token"
 
 # first app
-CONSUMER_KEY="MtKiWMxQiGoMkerKq9blpg"
-CONSUMER_SECRET="ldBbmasgnqixO0XDoOmsRz218iMhyl3ubnvebbtxkpQ"
-ACCESS_TOKEN="2190727249-h0G0u3ZUbHRJ80qQIGekZG4mbP0bnzXhqvyGbNP"
-ACCESS_TOKEN_SECRET="q11BgBbI5ixbjHzJ80VeBiCnTZUGTC6olYCyH2O2Xuvus"
+CONSUMER_KEY="LwmgdHnaGllQShOVG0fQA"
+CONSUMER_SECRET="HUMx3ysLeqRKkuHR6Pk5TCJdq7Jm5V8NN3db8nQzLg"
+ACCESS_TOKEN="2207254310-gsfAp1t51mnNVwLDTFguBqUuKeiYgGtARxoas0b"
+ACCESS_TOKEN_SECRET="WVULbmKoP9bZOJ6lTe6hfMngEnsBF1Ech0Rp62LcGPZ2w"
 
-# second app
-ALTERNATE_CONSUMER_KEY="xQy1LLrzXXED56UqVt1uqg"
-ALTERNATE_CONSUMER_SECRET="LSCUB0J9FrgmyBBUn9sQcLHUIoGexBb4uRCngSnfV0"
-ALTERNATE_ACCESS_TOKEN="2190727249-qfMYHGRxBCufwjGbJquAX3Big8qpbiWBzXJVWcv"
-ALTERNATE_ACCESS_TOKEN_SECRET="yUZsxIDb0PtMCivHp1H9ofX9lyZH5tflLnPwYXTWw4EXW"
 
-username="Henry H. Lightcap"
+username="Something Will Fail"
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = CONSUMER_KEY
@@ -28,12 +23,6 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ACCESS_TOKEN_SECRET
 end
 
-backup_client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ALTERNATE_CONSUMER_KEY
-  config.consumer_secret     = ALTERNATE_CONSUMER_SECRET
-  config.access_token        = ALTERNATE_ACCESS_TOKEN
-  config.access_token_secret = ALTERNATE_ACCESS_TOKEN_SECRET
-end
 
 
 timeline=client.user_timeline(username, :count => 200)
